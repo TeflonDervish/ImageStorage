f905849706d90e905ccbc22a7958ec8c612420d7
teflondervish <slava.semenov03@mail.ru>
2025-04-19 11:49:21 +0500

    обновил конфигурацию файла для получения картинок

--- src/main/java/ru/semenov/ImageStorage/controller/ImageList.java
+++ src/main/java/ru/semenov/ImageStorage/controller/ImageList.java
@@ -1,11 +1,12 @@
 package ru.semenov.ImageStorage.controller;
 
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -17,19 +18,28 @@ import java.util.List;
 @Controller
 public class ImageList {
 
+    private static final Log log = LogFactory.getLog(ImageList.class);
+
     @Value("${file.storage}")
     private String folder;
+
+    @Value("${url.address}")
+    private String url;
+
+    @Value("${server.port}")
+    private String port;
 
     @GetMapping("/preview")
     public String preview(Model model) {
         try {
+            log.info(folder);
             Path uploadPath = Paths.get(folder);
             List<String> imageUrls = new ArrayList<>();
 
             // Получаем список всех файлов в директории
             Files.list(uploadPath).forEach(file -> {
                 String fileName = file.getFileName().toString();
-                String fileUrl = "http://212.192.217.207:8090/api/images/" + fileName;
+                String fileUrl = "http://" + url + ":" + port +"/api/images/" + fileName;
                 imageUrls.add(fileUrl);
             });
 
